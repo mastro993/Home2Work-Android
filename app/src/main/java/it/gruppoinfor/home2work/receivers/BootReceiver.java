9c99b570d390c59ac4e50b05cb0c31c773ae2ea9
Federico Mastrini <mastro993@users.noreply.github.com>
2017-10-29 11:51:52 +0100

    Notifica

--- app/src/main/java/it/gruppoinfor/home2work/receivers/BootReceiver.java
+++ app/src/main/java/it/gruppoinfor/home2work/receivers/BootReceiver.java
@@ -1,6 +1,7 @@
 package it.gruppoinfor.home2work.receivers;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 
 import it.gruppoinfor.home2work.R;
 import it.gruppoinfor.home2work.api.SessionManager;
@@ -63,19 +65,25 @@ public class BootReceiver extends BroadcastReceiver {
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        String channelID = "ROUTE_SERVICE_NOTIFICATION";
+        int notificationIcon = R.drawable.home2work_icon;
+
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Azione richiesta")
-                .setContentText("Per poter usare il servizio devi effettuare il login")
-                .setAutoCancel(true)
+        Notification notification = new NotificationCompat.Builder(context, channelID)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setSmallIcon(notificationIcon)
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                //.setLargeIcon(icon)
+                .setContentTitle("Home2Work")
+                .setContentText("Per poter utilizzare il servizio devi effettuare l'accesso")
                 .setSound(defaultSoundUri)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+                .build();
 
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(0, notification);
     }
 
     private void showEnableTrackingNotification() {
